06a818448290c0bb1b652912866591feb2aabe9b
Benedikt Vogler <e-mail@benediktvogler.de>
2013-12-14 06:19:27 +0100

    added empty render method

--- com/BombingGames/WeaponOfChoice/Weapon.java
+++ com/BombingGames/WeaponOfChoice/Weapon.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.BombingGames.WeaponOfChoice;
 
 /**
@@ -18,4 +12,7 @@ public class Weapon {
         this.id = id;
     }
     
+    public void renderBig(int x, int y){
+    
+    }
 }
