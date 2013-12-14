6ec8e8a4851973d55f209ff54bda0a028efe3be4
Benedikt Vogler <e-mail@benediktvogler.de>
2013-12-14 05:30:31 +0100

    Include Wurfel Engine V1.2

--- com/BombingGames/WeaponOfChoice/WeaponOfChoice.java
+++ com/BombingGames/WeaponOfChoice/WeaponOfChoice.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package com.BombingGames.WeaponOfChoice;
 
-package weaponofchoice;
+import com.BombingGames.WurfelEngine.WEMain;
 
 /**
  *
@@ -16,7 +12,7 @@ public class WeaponOfChoice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+                WEMain.construct("Wurfelengine V" + WEMain.VERSION, args); 
     }
     
 }
