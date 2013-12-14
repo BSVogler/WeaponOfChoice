fb0df924d7fa20bdb68cd2a429c9d687ed8a759e
Benedikt Vogler <e-mail@benediktvogler.de>
2013-12-14 21:27:37 +0100

    a single block to make the map more interesting

--- com/BombingGames/WurfelEngine/Core/Map/Chunk.java
+++ com/BombingGames/WurfelEngine/Core/Map/Chunk.java
@@ -129,6 +129,12 @@ public class Chunk {
                 int pillary = (int) (Math.random()*blocksY-1);
                 //pillar
                 for (int z=0; z < blocksZ-2; z++) data[pillarx][pillary][z] = new Cell(1);
+                
+                //single block
+                pillarx = (int) (Math.random()*blocksX-1);
+                pillary = (int) (Math.random()*blocksY-1);
+                //pillar
+                data[pillarx][pillary][blocksZ/2] = new Cell(4);
                 break;
             }
                 
