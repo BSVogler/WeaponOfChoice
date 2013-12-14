56e8ce66a76d9b292a00983fd04190d355124ef5
Benedikt Vogler <e-mail@benediktvogler.de>
2013-12-14 06:52:07 +0100

    Added Spritehseet

--- com/BombingGames/WeaponOfChoice/Weapon.java
+++ com/BombingGames/WeaponOfChoice/Weapon.java
@@ -1,18 +1,44 @@
 package com.BombingGames.WeaponOfChoice;
 
+import com.BombingGames.WurfelEngine.Core.View;
+import com.BombingGames.WurfelEngine.WEMain;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
 /**
  *
  * @author Benedikt Vogler
  */
 public class Weapon {
     private String Name;
-    private int id;
+    private final int id;
+    private static TextureAtlas spritesheetBig;
 
+    public static void initClass(){
+        spritesheetBig = WEMain.getInstance().manager.get("com/BombingGames/WeaponOfChoice/SpritesheetBig.txt");
+    }
+    
     public Weapon(int id) {
         this.id = id;
     }
     
-    public void renderBig(int x, int y){
+    /**
+     * Renders a big version of the image
+     * @param view
+     * @param x
+     * @param y 
+     */
+    public void renderBig(View view, int x, int y){
+        Sprite sprite = new Sprite(spritesheetBig.findRegion(""+id));
+        sprite.setX(x);
+        sprite.setY(y);
+        sprite.draw(view.getBatch());
     
     }
+
+    public static TextureAtlas getSpritesheetBig() {
+        return spritesheetBig;
+    }
+    
+    
 }
