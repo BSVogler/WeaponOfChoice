50adf760db622d00961d2beb10c9f7362651ece4
Benedikt Vogler <e-mail@benediktvogler.de>
2014-02-23 19:14:14 +0100

    Added entity factory

--- com/BombingGames/WeaponOfChoice/CustomConfiguration.java
+++ com/BombingGames/WeaponOfChoice/CustomConfiguration.java
@@ -1,12 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.BombingGames.WeaponOfChoice;
 
 import com.BombingGames.WurfelEngine.Configuration;
+import com.BombingGames.WurfelEngine.Core.Gameobjects.AbstractEntity;
+import com.BombingGames.WurfelEngine.Core.Gameobjects.AnimatedBlock;
+import com.BombingGames.WurfelEngine.Core.Gameobjects.Block;
+import com.BombingGames.WurfelEngine.Core.Gameobjects.EntityFactory;
+import com.BombingGames.WurfelEngine.Core.Gameobjects.EntitySpawner;
+import com.BombingGames.WurfelEngine.Core.Gameobjects.ExplosiveBarrel;
+import com.BombingGames.WurfelEngine.Core.Gameobjects.SimpleEntity;
+import com.BombingGames.WurfelEngine.Core.Map.Point;
+import com.badlogic.gdx.Gdx;
 
 /**
  *
@@ -18,5 +21,30 @@ public class CustomConfiguration extends Configuration {
     public int getChunkGenerator() {
         return 2;
     }
+
+    @Override
+    public EntityFactory getEntityFactory() {
+        return new CustomEntityFactory(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static class CustomEntityFactory implements EntityFactory {
+
+        @Override
+        public AbstractEntity produce(int id, int value, Point point) {
+            AbstractEntity entity;
+            switch (id){
+                //...
+                case 40:
+                    entity = new Enemy(id, point);
+                    break;
+                default:
+                    entity = new SimpleEntity(id);
+                    Gdx.app.error("CustomBlockFactory", "Entity not defined.");
+            }
+            return entity;
+        }
+    }
+    
+    
     
 }
